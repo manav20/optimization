794ee4a59116d80a78e887e95f19329737a63040
manav20 <manav.wadhawan94@gmail.com>
2021-01-01 12:15:11 -0500

    chamges variable name for better readability

--- src/optimization.cpp
+++ src/optimization.cpp
@@ -15,7 +15,6 @@
 
 
 
-
     @file: optmization.cpp
     @author: Manav
 */
@@ -37,11 +36,11 @@ Opt::Opt(double maximum_iteration) {
 }
 
 
-bool Opt::CheckStop(const double& iter, const vector<double>& g_curr) {
+bool Opt::CheckStop(const double& iter, const vector<double>& gradient) {
     const double epsilon_x = 1.0000e-4;
     const double epsilon_g = 1.0000e-5;   
     bool stop_flag = false;
-    if (norm(g_curr) < epsilon_g) { 
+    if (norm(gradient) < epsilon_g) { 
         cout << "Stopped due to low gradient\n";
         stop_flag = true;
     }
@@ -58,46 +57,47 @@ void Opt::DisplayIteration(double iter_no, double x_1, double x_2, double step_s
     cout << iter_no << '\t' <<  x_1 << '\t' << x_2  << '\t' << rosenb_f({x_1, x_2}) << '\t' << step_size << '\n';
 }
 
-vector<double> Opt::newton(const vector<double>& x0, const vector<double>& g_curr) {
+vector<double> Opt::newton(const vector<double>& x0, const vector<double>& gradient) {
     vector<vector<double>> F_curr, F_curr_inv;
     F_curr = rosenb_d2f({x0[0], x0[1]});
     F_curr_inv = inverse_mat(F_curr); 
-    vector<double> d_curr(2, 0);
-    d_curr[0] = -1 * (F_curr_inv[0][0]*g_curr[0] + F_curr_inv[0][1]*g_curr[1]);
-    d_curr[1] = -1 * (F_curr_inv[1][0]*g_curr[0] + F_curr_inv[1][1]*g_curr[1]);
-    return d_curr;
+    vector<double> direction(2, 0);
+    direction[0] = -1 * (F_curr_inv[0][0]*gradient[0] + F_curr_inv[0][1]*gradient[1]);
+    direction[1] = -1 * (F_curr_inv[1][0]*gradient[0] + F_curr_inv[1][1]*gradient[1]);
+    return direction;
 }
 
-vector<double> Opt::steep_desc(const vector<double>& g_curr) {
-    vector<double> d_curr(2, 0);
-    d_curr[0] = -g_curr[0];
-    d_curr[1] = -g_curr[1];
-    return d_curr;
+vector<double> Opt::steep_desc(const vector<double>& gradient) {
+    vector<double> direction(2, 0);
+    direction[0] = -gradient[0];
+    direction[1] = -gradient[1];
+    return direction;
 }
 
 
-void Opt::optimize(const vector<double>& x0, const string& algo) {
+void Opt::optimize(const vector<double>& x0, const string& algorithm_choice) {
     double iter_no = 1;
     double x_curr_1, x_curr_2;
-    vector<double> d_curr(2, 0);
+    vector<double> direction(2, 0);
     double x_new_1 = x0[0], x_new_2 = x0[1];
    
     while(iter_no++) {
         x_curr_1 = x_new_1;
         x_curr_2 = x_new_2;
-        vector<double> g_curr = rosenb_grad({x_curr_1, x_curr_2});
-        if (algo == "newtons_algorithm")
-            d_curr = newton({x_curr_1, x_curr_2}, g_curr);
-        else if (algo == "steepest_descent")
-            d_curr = steep_desc(g_curr);
-        double alpha = backtracking(x_curr_1, x_curr_2, d_curr, g_curr);
-        x_new_1 = x_curr_1 + alpha*d_curr[0];
-        x_new_2 = x_curr_2 + alpha*d_curr[1];
-        double step_size = alpha*norm(d_curr);
+        vector<double> gradient = rosenb_grad({x_curr_1, x_curr_2});
+        if (algorithm_choice == "newtons_algorithm")
+            direction = newton({x_curr_1, x_curr_2}, gradient);
+        else if (algorithm_choice == "steepest_descent")
+            direction = steep_desc(gradient);
+        double alpha = backtracking(x_curr_1, x_curr_2, direction, gradient);
+        x_new_1 = x_curr_1 + alpha*direction[0];
+        x_new_2 = x_curr_2 + alpha*direction[1];
+        double step_size = alpha*norm(gradient);
         DisplayIteration(iter_no, x_new_1, x_new_2, step_size);
-        if (CheckStop(iter_no, g_curr))  break;
+        if (CheckStop(iter_no, gradient))  break;
     }   
 }
+
 
 
 
--- src/optimization.hpp
+++ src/optimization.hpp
@@ -9,11 +9,11 @@ class Opt
     public:
         double max_iter;
         Opt(double maximum_iteration);
-        bool CheckStop(const double& iter, const std::vector<double>& g_curr);
+        bool CheckStop(const double& iter, const std::vector<double>& gradient);
         void DisplayIteration(double iter_no, double x_1, double x_2, double step_size) const;
-        std::vector<double> newton(const std::vector<double>& x0, const std::vector<double>& g_curr);
-        std::vector<double> steep_desc(const std::vector<double>& g_curr);
-        void optimize(const std::vector<double>& x0, const std::string& algo);
+        std::vector<double> newton(const std::vector<double>& x0, const std::vector<double>& gradient);
+        std::vector<double> steep_desc(const std::vector<double>& gradient);
+        void optimize(const std::vector<double>& x0, const std::string& algorithm_choice);
 };
 
 #endif // OPTIMIZATION_HPP_
